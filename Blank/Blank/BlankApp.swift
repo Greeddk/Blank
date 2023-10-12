1aa8a17b52ca6998232bb6b0ea607aec57d9264c
Suppppppp <u1316@daum.net>
2023-10-12 15:47:04 +0900

    [Setting] xcode 기본세팅

---
+++ Blank/Blank/BlankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlankApp.swift
+//  Blank
+//
+//  Created by Sup on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BlankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
