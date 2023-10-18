8336e1aa9f76190eb53b919e3adcd745d41f278f
윤범태 <yoonbumtae@gmail.com>
2023-10-18 18:00:48 +0900

    [Fix] PDF생성시 이미지 병합 로직 수정

--- Blank/Blank/Util/PDFUtil.swift
+++ Blank/Blank/Util/PDFUtil.swift
@@ -16,31 +16,25 @@ func pageCount(of url: URL) -> Int? {
     PDFDocument(url: url)?.pageCount
 }
 
+/// 이미지 여러장을 순서대로 조합해 PDF 데이터 생성
+/// https://stackoverflow.com/questions/10486888/convert-multiple-images-as-pdf-with-multiple-page
+/// - Parameters:
+///   - images: 이미지 배열 (순서대로)
 func createPDFFromUIImages(from images: [UIImage]) -> NSMutableData {
-    var pageHeight = 0.0
-    var pageWidth = 0.0
-    
-    // 페이지 사이즈 정하기
-    for image in images {
-        pageHeight = pageHeight + Double(image.size.height)
-        
-        if Double(image.size.width) > pageWidth {
-            pageWidth = Double(image.size.width)
-        }
-    }
-    
     let pdfData = NSMutableData()
     let pdfConsumer = CGDataConsumer(data: pdfData as CFMutableData)!
     
-    var mediaBox = CGRect.init(x: 0, y: 0, width: pageWidth, height: pageHeight)
+    let maxWidth = images.max { $0.size.width > $1.size.width }
+    let maxHeight = images.max { $0.size.height > $1.size.height }
+    
+    var mediaBox = CGRect.init(x: 0, y: 0, width: 3000, height: 3000)
     let pdfContext = CGContext(consumer: pdfConsumer, mediaBox: &mediaBox, nil)!
     
     for image in images {
-        print(image, image.hash)
-        var mediaBox2 = CGRect.init(x: 0, y: 0, width: image.size.width, height: image.size.height)
+        var mediaBox = CGRect.init(x: 0, y: 0, width: image.size.width, height: image.size.height)
         
-        pdfContext.beginPage(mediaBox: &mediaBox2)
-        pdfContext.draw(image.cgImage!, in: CGRect.init(x: 0.0, y: 0, width: pageWidth, height: Double(image.size.height)))
+        pdfContext.beginPage(mediaBox: &mediaBox)
+        pdfContext.draw(image.cgImage!, in: CGRect.init(x: 0.0, y: 0, width: Double(image.size.width), height: Double(image.size.height)))
         pdfContext.endPage()
     }
     
