5aee248b946020713a9b984b43de4edd3757fd44
윤범태 <yoonbumtae@gmail.com>
2023-10-22 02:06:27 +0900

    [Feat] 코어데이터 서비스 인터페이스 설계

---
+++ Blank/Blank/CoreData/CDService.swift
@@ -0,0 +1,8 @@
+//
+//  CDService.swift
+//  Blank
+//
+//  Created by 윤범태 on 2023/10/20.
+//
+
+import Foundation
---
+++ Blank/Blank/Extension/Int+.swift
@@ -0,0 +1,8 @@
+//
+//  Int+.swift
+//  Blank
+//
+//  Created by 윤범태 on 2023/10/22.
+//
+
+import Foundation
