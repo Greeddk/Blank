9377590a04e0301613e88d9556f026f2d1cf53db
Greed <suby0475@gmail.com>
2023-10-31 17:16:17 +0900

    [Feat] pop to Root 기능 구현

---
+++ Blank/Blank/Util/NavigatioinUtil.swift
@@ -0,0 +1,34 @@
+//
+//  NavigatioinUtil.swift
+//  Blank
+//
+//  Created by Greed on 10/31/23.
+//
+
+import SwiftUI
+
+struct NavigationUtil {
+    static func popToRootView(animated: Bool = false) {
+        findNavigationController(viewController: UIApplication.shared.connectedScenes.flatMap { ($0 as? UIWindowScene)?.windows ?? [] }.first { $0.isKeyWindow }?.rootViewController)?.popToRootViewController(animated: animated)
+    }
+    
+    static func findNavigationController(viewController: UIViewController?) -> UINavigationController? {
+        guard let viewController = viewController else {
+            return nil
+        }
+        
+        if let navigationController = viewController as? UITabBarController {
+            return findNavigationController(viewController: navigationController.selectedViewController)
+        }
+        
+        if let navigationController = viewController as? UINavigationController {
+            return navigationController
+        }
+        
+        for childViewController in viewController.children {
+            return findNavigationController(viewController: childViewController)
+        }
+        
+        return nil
+    }
+}
