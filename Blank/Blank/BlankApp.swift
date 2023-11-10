f3ebc0cb22380a14bcd2e0a28dd7667bac6d404f
윤범태 <yoonbumtae@gmail.com>
2023-11-10 16:59:16 +0900

    [Chore] 샘플 파일 삭제

--- Blank/Blank/BlankApp.swift
+++ Blank/Blank/BlankApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct BlankApp: App {
     init() {
-        HomeViewModel.copySampleFiles()
+        // HomeViewModel.copySampleFiles()
     }
     
     var body: some Scene {
