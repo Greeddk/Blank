80b770e7b336b76fbeb51afbd65c3085803169ee
윤범태 <yoonbumtae@gmail.com>
2023-11-11 18:18:38 +0900

    [Fix] 파일이 한개일 때 도큐먼트의 파일을 추가하면 파일이 삭제되는 현상

--- Blank/Blank/Extension/FileMananger+.swift
+++ Blank/Blank/Extension/FileMananger+.swift
@@ -11,13 +11,9 @@ extension FileManager {
     /// 파일을 복사하는 기능
     public func secureCopyItem(at srcURL: URL, to dstURL: URL) -> Bool {
         do {
-            if FileManager.default.fileExists(atPath: dstURL.path) {
-                try FileManager.default.removeItem(at: dstURL)
-            }
-            
             try FileManager.default.copyItem(at: srcURL, to: dstURL)
         } catch (let error) {
-            print("Cannot copy item at \(srcURL) to \(dstURL): \(error)")
+            print("[DEBUG] Cannot copy item at \(srcURL) to \(dstURL): \(error)")
             return false
         }
         
