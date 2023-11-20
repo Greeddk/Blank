43812e99301d2dca2f03cb136e01c01775a0ed2c
윤범태 <yoonbumtae@gmail.com>
2023-11-20 21:54:18 +0900

    [Fix] WordSelectView: 터치(손가락)과 펜슬 동작 분리

---
+++ Blank/Blank/View/Common/TouchRepresentedView.swift
@@ -0,0 +1,18 @@
+//
+//  TouchRepresentedView.swift
+//  Blank
+//
+//  Created by 윤범태 on 2023/11/20.
+//
+
+import SwiftUI
+
+struct TouchRepresentedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TouchRepresentedView()
+}
